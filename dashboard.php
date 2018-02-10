86d0000b2aa32340e8ebdbd2e094aa68b728decd
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-02-10 19:46:59 +0000

    Fix 4

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <?php
 include('session.php');
 
-$sql = "SELECT userID, username, password, firstname, lastname, email FROM User WHERE userID = '$_SESSION[login_user]'";
+$sql = "SELECT CustomerID, Username, Password, FirstName, LastName, Email, QuestionAnswer, DOB FROM User WHERE customerID = '$_SESSION[login_user]'";
 $result = mysqli_query($conn,$sql);
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 mysqli_close($conn);
@@ -9,15 +9,17 @@ mysqli_close($conn);
 <html>
 
 <head>
-<title>Dashboard</title>
+<title>Test Dashboard</title>
 </head>
 
 <body>
-<h1>Welcome <?php echo $row["firstname"]; ?></h1>
-<?php echo "Username: " . $row["username"] . "</br>"?>
-<?php echo "Firstname: " . $row["firstname"] . "</br>"?>
-<?php echo "Lastname: " . $row["lastname"] . "</br>"?>
-<?php echo "Email: " . $row["email"] . "</br>"?>
+<h1>Welcome <?php echo $row["FirstName"]; ?></h1>
+<?php echo "Username: " . $row["Username"] . "</br>"?>
+<?php echo "FirstName: " . $row["FirstName"] . "</br>"?>
+<?php echo "LastName: " . $row["LastName"] . "</br>"?>
+<?php echo "Email: " . $row["Email"] . "</br>"?>
+<?php echo "QuestionAnswer: " . $row["QuestionAnswer"] . "</br>"?>
+<?php echo "DOB: " . $row["DOB"] . "</br>"?>
 <h2><a href="update.php">Update Details</a></h2>
 <h2><a href = "login.php">Sign Out</a></h2>
 
