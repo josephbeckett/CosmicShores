dbbf5d1c476d5b0d06fcdfcc32dd55655a6e78ab
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-02-10 18:29:26 +0000

    Indenting

--- signup.php
+++ signup.php
@@ -5,21 +5,21 @@ session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 // username and password sent from form
 
-$myfirstname = mysqli_real_escape_string($conn,$_POST["txtFirsName"]);
-$mylastname = mysqli_real_escape_string($conn,$_POST["txtLastName"]);
-$myusername = mysqli_real_escape_string($conn,$_POST["txtUsername"]);
-$mypassword = mysqli_real_escape_string($conn,$_POST["txtPassword"]);
-$myemail = mysqli_real_escape_string($conn,$_POST["txtEmail"]);
-$myQuestionAnswer = mysqli_real_escape_string($conn,$_POST["txtQuestionAnswer"]);
-$myDOB = mysqli_real_escape_string($conn,$_POST["txtDOB"]);
+    $myfirstname = mysqli_real_escape_string($conn,$_POST["txtFirsName"]);
+    $mylastname = mysqli_real_escape_string($conn,$_POST["txtLastName"]);
+    $myusername = mysqli_real_escape_string($conn,$_POST["txtUsername"]);
+    $mypassword = mysqli_real_escape_string($conn,$_POST["txtPassword"]);
+    $myemail = mysqli_real_escape_string($conn,$_POST["txtEmail"]);
+    $myQuestionAnswer = mysqli_real_escape_string($conn,$_POST["txtQuestionAnswer"]);
+    $myDOB = mysqli_real_escape_string($conn,$_POST["txtDOB"]);
 
-$sql = "INSERT INTO User (FirstName, LastName, Username, Password, Email, QuestionAnswer, DOB)
-VALUES ('$myfirsdsaftname', '$mylastname', '$myusername', '$mypassword', '$myemail', '$myQuestionAnswer', '$myDOB')";
-if (mysqli_query($conn, $sql)) {
-$info = "User Created Successfully!";
-} else {
-$info ="Unable to Add User!";
-}
+    $sql = "INSERT INTO User (FirstName, LastName, Username, Password, Email, QuestionAnswer, DOB)
+    VALUES ('$myfirsdsaftname', '$mylastname', '$myusername', '$mypassword', '$myemail', '$myQuestionAnswer', '$myDOB')";
+    if (mysqli_query($conn, $sql)) {
+        $info = "User Created Successfully!";
+    } else {
+        $info ="Unable to Add User!";
+    }
 }
 
 ?>
