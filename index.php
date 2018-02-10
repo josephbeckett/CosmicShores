74575f829125c67c805ea699629b8cc13b21d079
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-02-10 17:00:22 +0000

    Login Work1

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
 include("config.php")
-include("login.php")
+session_start();
 
-
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+   // username and password sent from form 
+   
+   $myusername = mysqli_real_escape_string($conn,$_POST["txtUsername"]);
+   $mypassword = mysqli_real_escape_string($conn,$_POST["txtPassword"]); 
+   
+   $sql = "SELECT CustomerID FROM User WHERE Username = '$myusername' and Password = '$mypassword'";
+   $result = mysqli_query($conn,$sql);
+   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+   $active = $row["active"];
+   
+   $count = mysqli_num_rows($result);
+   
+   // If result matched $myusername and $mypassword, table row must be 1 row
+     
+   if($count == 1) {
+      $_SESSION["login_user"] = $row["CustomerID"];
+      
+      header("location: dashboard.php");
+   }else {
+      $info = "Your Login Name or Password is invalid";
+   }
+}
+mysqli_close($conn);
 ?>
 <html>
   <head>
@@ -78,7 +101,7 @@ include("login.php")
     <!-- Modal Structure -->
     <div id="modal1" class="modal font">
         <div class="modal-content">
-			<div class="container">
+			<div class="container" method="post">
 				<div class="input-field">
 					<input type="text" name="txtUsername" required>
 					<label for="txtUsername">Username</label>
