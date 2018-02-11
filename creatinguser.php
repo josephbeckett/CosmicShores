56d7c9bbf478808f995d2cde3d456fe54a2c8d3f
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-02-11 12:59:36 +0000

    Fix 23

--- creatinguser.php
+++ creatinguser.php
@@ -5,7 +5,7 @@ $info = "";
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 // username and password sent from form
 
-    $myfirstname = mysqli_real_escape_string($conn,$_POST["txtFirsName"]);
+    $myfirstname = mysqli_real_escape_string($conn,$_POST["txtFirstName"]);
     $mylastname = mysqli_real_escape_string($conn,$_POST["txtLastName"]);
     $myusername = mysqli_real_escape_string($conn,$_POST["txtUsername"]);
     $mypassword = mysqli_real_escape_string($conn,$_POST["txtPassword"]);
