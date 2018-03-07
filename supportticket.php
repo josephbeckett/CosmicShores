705ed5bbfdbdd9584546637c766d58716cf34483
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-03-07 20:28:51 +0000

    IT WILL WORK

--- supportticket.php
+++ supportticket.php
@@ -7,7 +7,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $myproblemtitle = mysqli_real_escape_string($conn,$_POST["txtProblemTitle"]);
     $myproblemsubtitle = mysqli_real_escape_string($conn,$_POST["txtProblemSubtitle"]);
     $myproblemcontent = mysqli_real_escape_string($conn,$_POST["txtProblemContent"]);
-    $myvalidation = mysql_real_escape_string($conn,$POST_["txtValidation"]);
+    $myvalidation = mysql_real_escape_string($conn,$_POST["txtValidation"]);
     //$mycurrentdate = date('Y-m-d H:i:s');
 
     $sql = "INSERT INTO Support (PostTitle, PostDescription, PostContent, Validation)
