5e9f4d11408d38330bd46bb72bbeeae8c944148a
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-03-07 20:19:03 +0000

    Might work

--- supportticket.php
+++ supportticket.php
@@ -1,16 +1,16 @@
 <?php
-include("config.php");
 $info = "";
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-// username and password sent from form
+// Support tickets are sent from form
 
     $myproblemtitle = mysqli_real_escape_string($conn,$_POST["txtProblemTitle"]);
     $myproblemsubtitle = mysqli_real_escape_string($conn,$_POST["txtProblemSubtitle"]);
     $myproblemcontent = mysqli_real_escape_string($conn,$_POST["txtProblemContent"]);
     $myvalidation = mysql_real_escape_string($conn,$POST_["txtValidation"]);
-    
-    $sql = "INSERT INTO Support (SupportID, PostTitle, PostDescription, PostContent, Validation, PostDate)
-    VALUES ('$myproblemtitle', '$myproblemsubtitle', '$myproblemcontent', '$myvalidation')";
+    $mycurrentdate = date('Y-m-d H:i:s');
+
+    $sql = "INSERT INTO Support (PostTitle, PostDescription, PostContent, Validation, PostDate)
+    VALUES ('$myproblemtitle', '$myproblemsubtitle', '$myproblemcontent', '$myvalidation', '$mycurrentdate')";
     if (mysqli_query($conn, $sql)) {
         $info = "Ticket successfully submitted";
     } else {
