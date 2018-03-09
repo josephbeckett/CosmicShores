e5e1fff8fcc3d0d1bf04cc01b8d9cd4b83709e5c
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-03-09 16:06:08 +0000

    Posting date on support message

--- supportticket.php
+++ supportticket.php
@@ -9,10 +9,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $myproblemsubtitle = mysqli_real_escape_string($conn,$_POST["txtProblemSubtitle"]);
     $myproblemcontent = mysqli_real_escape_string($conn,$_POST["txtProblemContent"]);
     $myvalidation = mysqli_real_escape_string($conn,$_POST["txtProblemValidation"]);
-    //$mycurrentdate = date('Y-m-d H:i:s');
+    $mycurrentdate = date('Y-m-d H:i:s');
 
-    $sql = "INSERT INTO Support (PostTitle, PostDescription, PostContent, PostValidation)
-    VALUES ('$myproblemtitle', '$myproblemsubtitle', '$myproblemcontent', '$myvalidation')";
+    $sql = "INSERT INTO Support (PostTitle, PostDescription, PostContent, PostValidation, PostDate)
+    VALUES ('$myproblemtitle', '$myproblemsubtitle', '$myproblemcontent', '$myvalidation', '$mycurrentdate')";
     if (mysqli_query($conn, $sql)) {
         $info = "Ticket successfully submitted";
     } else {
