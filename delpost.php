04300c4f4aedb2f6b12156b74109848501cb68ef
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-03-13 13:28:57 +0000

    not sure what to do again

--- delpost.php
+++ delpost.php
@@ -1,18 +1,20 @@
 <?php 
-    session_start();
-    include_once("config.php");
 
-    if(!isset($_SESSION['Username'])) {
-        header("Location: login.php");
-        return;
-    }
+session_start();
+include_once('config.php');
 
-    if(!isset($_GET['pid'])) {
-        header("Location: blog.php");
-    } else {
-        $pid = $_GET['pid'];
-        $sql = "DELETE FROM Blog WHERE PostID = $pid";
-        mysqli_query($conn, $sql);
-        header("Location: blog.php");
-    }
+if (!isset($_SESSION['Username'])) {
+    header('Location: login.php');
+
+    return;
+}
+
+if (!isset($_GET['pid'])) {
+    header('Location: blog.php');
+} else {
+    $pid = $_GET['pid'];
+    $sql = "DELETE FROM Blog WHERE PostID = $pid";
+    mysqli_query($conn, $sql);
+    header('Location: blog.php');
+}
 ?>
