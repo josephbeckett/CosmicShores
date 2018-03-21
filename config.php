d1f93f841dbd1341642d2bceb48f6b825e66518c
wolflint <marcinwolf363@gmail.com>
2018-03-21 11:53:54 +0000

    Fun

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
    $servername = "localhost:3306";
-   $username = "root";
-   $password = "";
-   $dbname = "CosmicShores";
+   $username = "JosephBeckett";
+   $password = "Password123";
+   $dbname = "CosmicShoresDatabase";
    $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
