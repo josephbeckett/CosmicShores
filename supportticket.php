79db71ac77349da8ffce23bb23b0c8f797ab0dad
Joseph Beckett <b1730124@student.southstaffs.ac.uk>
2018-03-21 13:45:28 +0000

    Fun

--- supportticket.php
+++ supportticket.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])) {
     $myvalidation = mysqli_real_escape_string($conn,$_POST["txtProblemValidation"]);
     $mycurrentdate = date('Y-m-d H:i:s');
 
-    $sql = "INSERT INTO support (PostTitle, PostDescription, PostContent, PostValidation, PostDate)
+    $sql = "INSERT INTO Support (PostTitle, PostDescription, PostContent, PostValidation, PostDate)
     VALUES ('$myproblemtitle', '$myproblemsubtitle', '$myproblemcontent', '$myvalidation', '$mycurrentdate')";
     if (mysqli_query($conn, $sql)) {
         echo "Ticket successfully submitted";
