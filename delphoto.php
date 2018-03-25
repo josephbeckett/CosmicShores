9810632857dbe62b6337e6c05d1cdda996094db1
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-03-26 00:26:49 +0100

    probs not gonna work

--- delphoto.php
+++ delphoto.php
@@ -4,9 +4,24 @@
     if (!isset($_GET['photoid'])) {
         header('Location: gallery.php');
     } else {
+        $imgsql = "SELECT Image FROM Photos WHERE ImageID = $photoid"
+        $image = $row['Image']
+        $file = "uploadedimages/$photoid";
+        if (!unlink($file))
+          {
+          echo ("Error deleting $file");
+          }
+        else
+          {
+          echo ("Deleted $file");
+          }
         $photoid = $_GET['photoid'];
         $sql = "DELETE FROM Photos WHERE ImageID = $photoid";
         mysqli_query($conn, $sql);
+
+
         header('Location: gallery.php');
     }
+
+
 ?>
