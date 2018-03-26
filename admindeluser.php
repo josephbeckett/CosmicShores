7698a7633e319bcedaf2350d88a49a61b414c1bf
JosephBeckett <joseph.beckett1999@gmail.com>
2018-03-26 14:21:34 +0100

    mememems

--- admindeluser.php
+++ admindeluser.php
@@ -2,11 +2,11 @@
     include("session.php");
 
     if (!isset($_GET['uid'])) {
-        header('Location: admin.php');
+        header('Location: adminuserview.php');
     } else {
         $uid = $_GET['uid'];
         $sql = "DELETE FROM User WHERE CustomerID = $uid";
         mysqli_query($conn, $sql);
-        header('Location: admin.php');
+        header('Location: adminuserview.php');
     }
 ?>
