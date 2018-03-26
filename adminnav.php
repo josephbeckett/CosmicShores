60bb88d187b6b25c56aa7bb1b69ee5c47eea7d20
JosephBeckett <joseph.beckett1999@gmail.com>
2018-03-26 12:24:39 +0100

    what are you not working

--- adminnav.php
+++ adminnav.php
@@ -9,6 +9,8 @@ if ($_SESSION['admin'] != $row['Admin']) {
   <img class="logoPC" src="images/CosmicShoresLogoWithoutLogo.png" alt="">
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href='admin.php'>Support Tickets</a></li>
             <li><a href='adminuserview.php'>User Accounts List</a></li>
@@ -17,6 +19,8 @@ if ($_SESSION['admin'] != $row['Admin']) {
             <li><a href="logout.php">Sign out</a></li>
         </ul>
         <ul class="side-nav" id="mobile-demo">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href='admin.php'>Support Tickets</a></li>
             <li><a href='adminuserview.php'>User Accounts List</a></li>
