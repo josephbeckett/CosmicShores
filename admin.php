55535dab3f37321fa3a188771ecc2796e8babe4b
Joseph Beckett <joseph.beckett1999@gmail.com>
2018-03-28 08:33:56 +0100

    simple stuff

--- admin.php
+++ admin.php
@@ -53,8 +53,8 @@ $info = "";
           <h1 class="flow-text">Support Tickets</h1>
           <h4> <?php display_delsupportticket_message(); ?> </h4>
           <h4> <?php display_delallsupportticket_message(); ?> </h4>
-          <br />
           <a class="waves-effect waves-light btn" href='truncatesupport.php'>Delete All</a>
+          <br />
           <?php
             require_once("nbbc/nbbc.php");
 
