9e895d9658ee75f6f8d8bd56cd76140a7b8e2a1a
JosephBeckett <joseph.beckett1999@gmail.com>
2018-03-28 13:59:41 +0100

    Peak

--- loggingin.php
+++ loggingin.php
@@ -9,24 +9,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
    $myusername = mysqli_real_escape_string($conn, $_POST["txtUsername"]);
    $mypassword = mysqli_real_escape_string($conn, $_POST["txtPassword"]);
 
-   $sql = "SELECT * FROM User WHERE Username = '$myusername' and Password = '$mypassword' ";
+   $sql = "SELECT * FROM User WHERE Username = '$myusername'";
    $result = mysqli_query($conn,$sql);
-   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-   $customerid = $row['CustomerID'];
-   $customerusername = $row['Username'];
-   $customerpassword = $row['Password'];
-   $admin = $row['Admin'];
+   if($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+        $decryptpassword = password_verify($mypassword, $row['Password']);
+        if($decryptpassword == false) {
+          header("Location: login.php?login=error");
 
-    $count = mysqli_num_rows($result);
+        } elseif ($decryptpassword == true) {
+            $_SESSION['login_user'] = $row['CustomerID'];
+            $_SESSION['admin'] = $row['Admin'];
+            header("location: loginhomepage.php");
+        }
+   }
+   // $customerid = $row['CustomerID'];
+   // $customerusername = $row['Username'];
+   // $customerpassword = $row['Password'];
+   // $admin = $row['Admin'];
+
+    // $count = mysqli_num_rows($result);
    // If result matched $myusername and $mypassword, table row must be 1 row
 
-   if($count == 1) {
-      $_SESSION['login_user'] = $row['CustomerID'];
-      $_SESSION['admin'] = $row['Admin'];
-      header("location: loginhomepage.php");
-   } else {
-      echo "Your Login Name or Password is invalid";
-   }
+   // if($count == 1) {
+   //    $_SESSION['login_user'] = $row['CustomerID'];
+   //    $_SESSION['admin'] = $row['Admin'];
+   //    header("location: loginhomepage.php");
+   // } else {
+   //    echo "Your Login Name or Password is invalid";
+   // }
  }
     mysqli_close($conn);
 ?>
